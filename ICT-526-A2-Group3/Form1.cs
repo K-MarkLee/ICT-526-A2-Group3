df77a15ab55de112191b2bde1f4a6b8f67dc0198
Seungyeol Lee <vulca123@naver.com>
2020-10-07 12:10:48 +1300

    Hello World

--- ICT-526-A2-Group3/Form1.cs
+++ ICT-526-A2-Group3/Form1.cs
@@ -16,5 +16,10 @@ namespace ICT_526_A2_Group3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
